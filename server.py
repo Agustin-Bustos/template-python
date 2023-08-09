4ed8bc3f4f21f72b1efd126e259e7ce4516f0550
AgusBustos <bustosagustin103@gmail.com>
2023-08-08 23:29:50 -0300

    cambio al index.html

--- server.py
+++ server.py
@@ -1,6 +1,24 @@
+import psycopg2
 import os
 from flask import Flask, send_from_directory, render_template, redirect
 
+try:
+    connection=psycopg2.connect(
+        host='ep-odd-resonance-87921203.us-east-2.aws.neon.tech',
+        user='fl0user',
+        password='z1iKnXGNSw9e',
+        database='postgres'
+        #port='5432'
+    )
+    
+    print("conesion esitosa")
+    
+except Exception as ex:
+    print(ex) 
+    
+    
+    
+    
 app = Flask(__name__)
 
 port = int(os.environ.get("PORT", 5000))
